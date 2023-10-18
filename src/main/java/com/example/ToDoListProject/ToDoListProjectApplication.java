43aaaef7c4ec7754ee44e740c1e9d6498abc158e
HNK-hub <haticenur.4455@gmail.com>
2023-10-18 17:08:23 +0300

    ToDoList app created.

---
+++ src/main/java/com/example/ToDoListProject/ToDoListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToDoListProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListProjectApplication.class, args);
+	}
+
+}
